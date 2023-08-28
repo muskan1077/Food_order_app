9861df7f29eb8d514e042a2891fb95b0c8990f71
muskan1077 <muskan.1077gupta@gmail.com>
2023-08-28 23:06:58 +0530

    food_order_web

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import {Fragment} from 'react';
+
+import Header from './components/Layout/Header/Header';
+import Meals from './components/Meals/Meals';
+
+function App() {
+  return (
+    <Fragment>
+      <Header />
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
+  );
+}
+
+export default App;
---
+++ src/components/Layout/Header/HeaderCartButton.js
@@ -0,0 +1,16 @@
+import CartIcon from '../../Cart/CartIcon';
+import Classes from './HeaderCartButton.module.css';
+
+const HeaderCartButton = (props) => {
+    return (<button className = {Classes.button}>
+            <span className = {Classes.icon}>
+                <CartIcon />
+            </span>
+            <span>Your Cart</span>
+            <span classNmae = {Classes.badge}>
+                3
+            </span>
+        </button>
+    );
+};
+export default HeaderCartButton;
